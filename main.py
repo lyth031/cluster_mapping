3ecfb632a587ec91d9d957ec0092c9cd0cc09ec8
lyth031 <lyth031@outlook.com>
2018-10-15 13:59:26 +0800

    debug

--- main.py
+++ main.py
@@ -2,22 +2,24 @@ import renet
 import torch
 import time
 
-rwl = 3.9
-rbl = 3.9
-a = renet.Array()
-w = torch.zeros([32, 32])
-vin = 0.15*torch.ones([1, 32])
-rram = a.setValue(w)
-print(vin)
-ideal = a.idealOuput(vin)
-print(ideal*1e6)
-"""
-real = a.realOutput(vin)
-print(real*1e6)
-"""
+dtype = torch.float
+if torch.cuda.is_available():
+    device = torch.device("cuda: 0")
+else:
+    device = torch.device("cpu") 
+a = renet.Array(device = device)
+w = torch.ones([32, 32], device = device, dtype=torch.float)
 start = time.time()
-real_comparison = a.realOutputComparison(vin)
-print(real_comparison*1e6)
+for i in range(1):
+    vin = 0.15*torch.ones([1, 32], device = device, dtype=torch.float)
+    rram = a.setValue(w)
+    ideal = a.idealOuput(vin)
+    real = a.realOutput(vin)
+    print(real*1e6)
+    # real_comparison = a.realOutputComparison(vin)
+    # print(real - real_comparison)
+    # print(real_comparison*1e6)
+
 end = time.time()
 print (end-start)
 """
--- renet.py
+++ renet.py
@@ -5,15 +5,16 @@ class Array(object):
     """A nwl x nbl resistive cross-point array."""
 
     # Constructor
-    def __init__(self,nwl = 32, nbl = 32, rlrs = 50e3, rhrs = 500e3, rwl = 3.9, rbl = 3.9):
+    def __init__(self,nwl = 32, nbl = 32, rlrs = 50e3, rhrs = 500e3, rwl = 3.9, rbl = 3.9, device = "cpu"):
         self.nwl = nwl
         self.nbl = nbl
         self.rlrs = rlrs
         self.rhrs = rhrs
         self.rwl = rwl
         self.rbl = rbl
-        self.vnode = torch.zeros([2,self.nwl,self.nbl], dtype=torch.float)
-        self.rram = torch.zeros([self.nwl,self.nbl], dtype=torch.float)
+        self.device = device
+        self.vnode = torch.zeros([2,self.nwl,self.nbl], device = self.device, dtype=torch.float)
+        self.rram = torch.zeros([self.nwl,self.nbl], device = self.device, dtype=torch.float)
         
     def setValue(self, w):
         self.rram = 1 /( 1 / self.rhrs + (1 / self.rlrs - 1 / self.rhrs) * w)
@@ -30,13 +31,15 @@ class Array(object):
         error = 1
         count = 0
         w=4/(2+math.sqrt(4-(math.cos(math.pi/self.nwl)+math.cos(math.pi/self.nbl))**2))
-        left = 2 / self.rwl * torch.ones(self.nwl, self.nbl-1)
-        right = 1 / self.rwl * torch.ones(self.nwl, 1)       
-        relax_rwl = torch.cat((left, right),1) +  1 / self.rram
-        top = 1 / self.rbl * torch.ones(1, self.nbl)
-        bottom = 2 / self.rbl * torch.ones(self.nbl-1, self.nbl)
-        relax_rbl = torch.cat((top, bottom), 0) + 1 / self.rram
-        self.vnode[0] = torch.mm(vin.t(), torch.ones([1,self.nbl]))
+        left = 2 / self.rwl * torch.ones([self.nwl, self.nbl-1], device = self.device)
+        right = 1 / self.rwl * torch.ones([self.nwl, 1], device = self.device)       
+        parasitic_rwl = torch.cat((left, right),1) +  1 / self.rram
+        # print(parasitic_rwl)
+        top = 1 / self.rbl * torch.ones([1, self.nbl], device = self.device)
+        bottom = 2 / self.rbl * torch.ones([self.nwl-1, self.nbl], device = self.device)
+        parasitic_rbl = torch.cat((top, bottom), 0) + 1 / self.rram
+        # print(parasitic_rbl)
+        self.vnode[0] = torch.mm(vin.t(), torch.ones([1,self.nbl], device = self.device, dtype=torch.float))
 
         while error > tol and count <= max_step:
             error = 0
@@ -44,7 +47,7 @@ class Array(object):
             mid = (self.vnode[0, :, 0:self.nbl-2] + self.vnode[0, :, 2:self.nbl]) / self.rwl
             right = self.vnode[0, :, self.nbl-2:self.nbl-1] / self.rwl
             relax = torch.cat((left, mid ,right),1) + torch.div(self.vnode[1], self.rram)
-            relax = w * (torch.div(relax, relax_rwl) - self.vnode[0])
+            relax = (torch.div(relax, parasitic_rwl) - self.vnode[0])
             self.vnode[0] += relax
             m = relax.abs().max()
             if error <= m:
@@ -54,14 +57,14 @@ class Array(object):
             mid = (self.vnode[1, 0:self.nwl-2, :] + self.vnode[1, 2:self.nwl, :]) / self.rbl 
             bottom = self.vnode[1, self.nwl-2:self.nwl-1, :] / self.rbl
             relax = torch.cat((top, mid ,bottom),0) + torch.div(self.vnode[0], self.rram)
-            relax = w * (torch.div(relax, relax_rbl) - self.vnode[1])
+            relax = (torch.div(relax, parasitic_rbl) - self.vnode[1])
             self.vnode[1] += relax
             m = relax.abs().max()
             if error <= m:
                 error = m
             
             count += 1
-        print(self.vnode)
+        # print(count)
         vrram = self.vnode[0] - self.vnode[1]
         point_out = torch.div(vrram, self.rram)
         out = torch.sum(point_out,0)
@@ -74,8 +77,7 @@ class Array(object):
         error = 1
         count = 0
         w=4/(2+math.sqrt(4-(math.cos(math.pi/self.nwl)+math.cos(math.pi/self.nbl))**2))
-        self.vnode[0] = torch.mm(vin.t(), torch.ones([1,self.nbl]))
-        print(self.vnode)
+        self.vnode[0] = torch.mm(vin.t(), torch.ones([1,self.nbl], device = self.device, dtype=torch.float))
 
         while error > tol and count <= max_step:
             error = 0
